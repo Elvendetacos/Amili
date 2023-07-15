c0366de6def1c500821dfe790c1c59f6f50d35dc
Elvendetacos <reversdead@gmail.com>
2023-07-14 20:42:30 -0600

    placeholder Statistics view

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -6,15 +6,17 @@ import ProductKey from "../pages/productKey";
 import LandingPage from "../pages/landingPage";
 import History from "../pages/history";
 import { ProtectedRoutes } from "../components/protectedRoutes";
+import Statistics from "../pages/statistics";
 
 function Router() {
   return ( 
     <BrowserRouter>
       <Routes>
-        <Route element={<ProtectedRoutes/>}>
+        {/* <Route element={<ProtectedRoutes/>}> */}
           <Route path="/home" element={<Home/>}></Route>
           <Route path="/home/history" element={<History/>}></Route>
-        </Route>
+          <Route path="/home/statistics" element={<Statistics/>} ></Route>
+        {/* </Route> */}
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/register" element={<Register/>}></Route>
         <Route path="/key" element={<ProductKey/>}></Route>
